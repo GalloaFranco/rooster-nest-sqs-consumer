3f0bfa7c26654b16bb52c103523f9bd9320e8d07
Gallo <galloafranco@gmail.com>
2022-02-10 01:47:18 -0300

    ⏰ Remove cron and introduce recursive function

--- rooster-sqs-consumer/src/sqs-consumer.service.ts
+++ rooster-sqs-consumer/src/sqs-consumer.service.ts
@@ -7,40 +7,43 @@ import {
   ReceiveMessageCommandOutput,
   SQSClient,
 } from '@aws-sdk/client-sqs';
-import {Cron, CronExpression, SchedulerRegistry} from "@nestjs/schedule";
 
 @Injectable()
 export class SqsConsumerService {
   private readonly region = process.env.REGION || 'us-east-1';
   private sqsClient: SQSClient;
   private queueUrl: string;
-  private params: ReceiveMessageCommandInput;
-
-  constructor(private scheduleRegistry: SchedulerRegistry) {}
-
+  private isRunning: boolean;
 
   setupConsumer(queueUrl: string) {
     this.queueUrl = queueUrl;
-    this.sqsClient = new SQSClient({ region: this.region });
-    this.params = {
-      AttributeNames: ['SentTimestamp'],
-      MessageAttributeNames: ['All'],
-      QueueUrl: this.queueUrl,
-      VisibilityTimeout: 30,
-      WaitTimeSeconds: 10,
-    } as ReceiveMessageCommandInput;
+    this.isRunning = true;
+    this.sqsClient = new SQSClient({
+      region: this.region,
+      endpoint: 'http://localhost:4566', // TODO modify
+    });
+    this.isRunning = true;
   }
 
+  async listenQueue(
+    handlerFunction: (value: ReceiveMessageCommandOutput) => Promise<void>,
+  ) {
+    const params = {
+      AttributeNames: ["SentTimestamp"],
+      MessageAttributeNames: ["All"],
+      QueueUrl: this.queueUrl,
+      VisibilityTimeout: 20,
+      WaitTimeSeconds: 10,
+    } as ReceiveMessageCommandInput;
 
-  @Cron(CronExpression.EVERY_SECOND, {
-    name: 'rooster-sqs-consumer'
-  })
-  async listenQueue(handlerFunction: (value: ReceiveMessageCommandOutput) => Promise<void> ) {
-
-      const result = await this.sqsClient.send(
-        new ReceiveMessageCommand(this.params),
-      );
-      await handlerFunction(result);
+   while(this.isRunning) {
+     const result = await this.sqsClient.send(
+         new ReceiveMessageCommand(params),
+     );
+     if (result) {
+       await handlerFunction(result);
+     }
+   }
   }
 
   async deleteMessageFromQueue(deleteInput: DeleteMessageCommandInput) {
@@ -48,7 +51,7 @@ export class SqsConsumerService {
   }
 
   stopListeningQueue() {
-    this.scheduleRegistry.getCronJob('rooster-sqs-consumer').stop();
+    this.isRunning = false;
     this.sqsClient.destroy();
   }
 }
